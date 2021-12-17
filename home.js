a740ed2442f68757c9d47704f612b1feb27cd017
VECTOR3Studio <47158115+VECTOR3Studio@users.noreply.github.com>
2021-12-17 17:36:37 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,3 @@
+function changeBg(){
+    document.body.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+}
